e76cb0bcd6d9e4048f13d9889edd85f68bbc47a8
Leonardo Silva <leonardo.silva@modalgr.com.br>
2019-08-25 12:38:31 -0300

    Tela Profile

--- src/Navigator.js
+++ src/Navigator.js
@@ -4,6 +4,7 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 
 import Feed from './screens/Feed';
 import AddPhoto from './screens/AddPhoto';
+import Profile from './screens/Profile';
 
 const MenuRoutes = {
     Feed: {
@@ -26,7 +27,7 @@ const MenuRoutes = {
     },
     Profile: {
         name: 'Profile',
-        screen: Feed,
+        screen: Profile,
         navigationOptions: {
             title: 'Profile',
             tabBarIcon: ({ tintColor }) =>

456ba78de68f2ce13e9ef7fb8ff8fc40c8150334
Leonardo Silva <leonardo.silva@modalgr.com.br>
2019-08-25 02:44:04 -0300

    Componente Autor

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, View, Image, Dimensions } from 'react-native';
+import Autor from './Autor';
 
 class Post extends React.Component {
     render() {
         return (
             <View style={styles.container}>
+                <Autor email="leeohx.x@hotmail.com" autor="Leonardo Silva" />
                 <Image source={this.props.image} style={styles.image} />
             </View>
         )

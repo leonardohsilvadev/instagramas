8107eca9c38a299d76dd9b9ee5ffa94fa7a4f033
Leonardo Silva <leonardo.silva@modalgr.com.br>
2019-08-25 02:27:47 -0300

    Commit Componente Post

--- App.js
+++ App.js
@@ -6,11 +6,18 @@ import {
   View
 } from 'react-native';
 import Header from './src/components/Header';
+import Post from './src/components/Post';
 
 export default class App extends Component {
   render() {
     return (
+      <View style={{ flex: 1 }}>
+      
       <Header />
+      
+      <Post image={require('./assets/imgs/fence.jpg')} />
+      
+      </View>
     );
   }
 }
